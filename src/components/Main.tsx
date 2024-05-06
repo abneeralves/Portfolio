e8fa7ef152cf90f4a11c770deb5aac5b0d61920a
abneralvesmuniz76@gmail.com <abneralvesmuniz76@gmail.com>
2024-05-06 19:02:14 -0300

    Editando sobre mim

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,8 +3,20 @@ import FtAbner from '../../public/FtAbner.jpg'
 
 export default function Main () {
     return (
-        <main className=" flex justify-center bg-blue-600 h-full">
-            <h2 className="  text-white text-5xl mt-32">Sobre Mim</h2>
+        <main className="flex flex-col items-center bg-blue-600 h-full">
+            <div className=''>
+                <h2 className="text-white text-5xl mx-auto my-24">Sobre Mim</h2>
+            </div>
+            
+            <div className='grid grid-cols-3 gap-4 bg-zinc-200 h-4/6 w-5/6 rounded-2xl'>
+                <div className='col-span-1 flex items-center justify-center p-5'>
+                    <Image className='w-full h-full rounded-xl' src={FtAbner} alt='Foto Abner'/>
+                </div>
+
+                <div className='col-span-2'>
+
+                </div>
+            </div>
         </main>
     )
 }
