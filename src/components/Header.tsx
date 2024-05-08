80ad4d2b83b308bc95f42e161e8939ff27c5cad1
abneralvesmuniz76@gmail.com <abneralvesmuniz76@gmail.com>
2024-05-07 22:41:49 -0300

    Modificações simples

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,10 @@ import BT from '../../public/BOOTSTRAP.png'
 import NEXT from '../../public/NEXT.png'
 import TAILWIND from '../../public/TAILWIND.png'
 import TS from '../../public/TS.png'
-import { Open_Sans, Comic_Neue } from 'next/font/google'
+import { Open_Sans, Bebas_Neue } from 'next/font/google'
 
 const OpenS = Open_Sans({subsets: ['latin']})
-const ComicN = Comic_Neue({subsets: ['latin'], weight: '400'})
+const BebasN = Bebas_Neue({subsets: ['latin'], weight: '400'})
 
 import React, { useEffect, useRef, useState } from 'react'
 
@@ -30,11 +30,13 @@ const AutoHideImage: React.FC<{ src: string; alt: string }> = ({ src, alt }) => 
 
 
     return (
-        <header className="h-screen bg-gray-900">
+        <header className="h-screen bg-gradient-to-b from-gray-900 to-zinc-950 ">
 
             <nav className="flex justify-between">
-                <div>
-                    <a href=""><Image className='mx-64 mt-28' src={LetraA} alt='Letra A' width={70} /></a>  
+                <div className='mx-64 mt-28 flex items-center hover:rotate-180 transition-transform duration-300'>
+                    <p className='text-sky-500 text-3xl -m-2'>&lt;</p>
+                    <a href=""><Image className='' src={LetraA} alt='Letra A' width={70} /></a>
+                    <p className='text-sky-500 text-3xl -m-1'>/&gt;</p>
                 </div>
 
                 <div>
@@ -49,9 +51,9 @@ const AutoHideImage: React.FC<{ src: string; alt: string }> = ({ src, alt }) => 
 
             <div className='flex items-center h-2/4 ml-64'>
                 <div>
-                    <p className={`text-3xl text-white italic ${OpenS.className}`}>Olá, meu nome é</p>
-                    <h1 className={`text-7xl mt-3 text-sky-300 ${ComicN.className}`}>&lt;Abner Alves /&gt;</h1>
-                    <p className={`text-3xl mt-3 text-white ${OpenS.className}`}>e eu sou Desenvolvedor Front-end.</p>
+                    <p className={`text-4xl text-white ${OpenS.className}`}>👋 Olá, meu nome é</p>
+                    <h1 className={`text-9xl mt-2 text-sky-500 ${BebasN.className}`}>Abner Alves.</h1>
+                    <p className={`text-3xl text-white ${OpenS.className}`}>e eu sou Estudante e Desenvolvedor Front-end.</p>
                 </div>
 
                
@@ -93,9 +95,9 @@ const AutoHideImage: React.FC<{ src: string; alt: string }> = ({ src, alt }) => 
             </div>
             
             <svg className='ondas' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path className='onda2' fill="#0099ff" fill-opacity="1" d="M0,0L30,37.3C60,75,120,149,180,154.7C240,160,300,96,360,74.7C420,53,480,75,540,101.3C600,128,660,160,720,154.7C780,149,840,107,900,90.7C960,75,1020,85,1080,96C1140,107,1200,117,1260,106.7C1320,96,1380,64,1410,48L1440,32L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
+            <path fill="#2563EB" fill-opacity="10000" d="M0,320L34.3,288C68.6,256,137,192,206,181.3C274.3,171,343,213,411,224C480,235,549,213,617,170.7C685.7,128,754,64,823,64C891.4,64,960,128,1029,160C1097.1,192,1166,192,1234,160C1302.9,128,1371,64,1406,32L1440,0L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
 
-            <path className='onda1' fill="#2563eb" fill-opacity="10000" d="M0,320L34.3,288C68.6,256,137,192,206,181.3C274.3,171,343,213,411,224C480,235,549,213,617,170.7C685.7,128,754,64,823,64C891.4,64,960,128,1029,160C1097.1,192,1166,192,1234,160C1302.9,128,1371,64,1406,32L1440,0L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+            
             </svg>
         </header>
     )
