6102363b1509189b2ee9bb55d067211764a9a1e7
abneralvesmuniz76@gmail.com <abneralvesmuniz76@gmail.com>
2024-05-18 23:11:45 -0300

    Adicionando grid projetos

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,14 +8,14 @@ export default function Main () {
     return (
         <main className="flex flex-col items-center justify-center bg-blue-600 h-full">
             
-            <div className='md:grid grid-cols-2 2xl:grid-cols-2 2xl:gap-4 bg-gray-900 lg:h-6/6 w-5/6 2xl:h-4/6 rounded-2xl hover:shadow-md hover:shadow-gray-300'>
-                <div className='col-span-1  flex items-center justify-center p-5'>
+            <div className='md:grid grid-cols-2 2xl:grid-cols-2 2xl:gap-4 bg-gray-900 lg:h-6/6 w-4/6  rounded-2xl hover:shadow-md hover:shadow-gray-300'>
+                <div className='col-span-1 flex items-center justify-center p-5'>
                     <Image className='w-full h-full 2xl:h-[625px] rounded-xl' src={FtAbner} alt='Foto Abner'/>
                 </div>
 
                 <div className='col-span-1 flex flex-col items-center mt-5'>
                     <h1 className={`text-white text-2xl xl:text-3xl 2xl:mt-5 ${Exo.className}`}>Deixa-me Apresentar.</h1>
-                    <p className={`text-white text-md lg:text-xl 2xl:text-2xl lg:mt-5 p-5 md:pl-0 2xl:mt-20 ${Exo.className}`}>Meu nome é Abner, um entusiasta da tecnologia de 19 anos, atualmente imerso no mundo do desenvolvimento front-end. Sou um estudante dedicado do 3º semestre na UNIVOVE, buscando constantemente expandir meus horizontes e mergulhar nas complexidades do desenvolvimento web.
+                    <p className={`text-white text-md lg:text-xl 2xl:text-2xl lg:mt-5 p-5 md:pl-0 2xl:mt-10 ${Exo.className}`}>Meu nome é Abner, um entusiasta da tecnologia de 19 anos, atualmente imerso no mundo do desenvolvimento front-end. Sou um estudante dedicado do 3º semestre na UNIVOVE, buscando constantemente expandir meus horizontes e mergulhar nas complexidades do desenvolvimento web.
                     <br />
                     <br />
                     Com um olhar curioso e uma paixão pela resolução de problemas, estou sempre pronto para abraçar novos desafios e aprender com cada experiência. Meu objetivo é não apenas dominar as tecnologias atuais, mas também explorar novas áreas, especialmente no desenvolvimento back-end.</p>
--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
+import Porscher from '../../public/imgPorsche.png'
 
 import { Exo_2 } from "next/font/google"
-
 const Exo = Exo_2({subsets: ['latin'], weight: '400'})
 
 export default function Projetos () {
@@ -12,10 +12,41 @@ export default function Projetos () {
                 <h1 className={`text-4xl text-white mt-10 ${Exo.className}`}>Projetos.</h1>
             </div>
 
-            <div className="grid">
+            <div className="flex flex-col items-center justify-center mt-20">
+                <ul className="grid grid-cols-3 gap-10">
+                    <li className="bg-gray-900 w-96 h-96 rounded-xl">
+                        <Image className="rounded-md" src={Porscher} alt="" />
+                        <h1 className="text-white text-2xl mt-5 ml-2">Site Porsche</h1>
+                        <p className="text-white">Descrição projeto</p>
+                    </li>
+                    
+                    <li className="bg-gray-900 w-96 h-96 rounded-xl">
+                        <h1 className="text-white">Nome do Projeto</h1>
+                        <p className="text-white">Descrição projeto</p>
+                    </li>
 
+                    <li className="bg-gray-900 w-96 h-96 rounded-xl">
+                        <h1 className="text-white">Nome do Projeto</h1>
+                        <p className="text-white">Descrição projeto</p>
+                    </li>
+
+                    <li className="bg-gray-900 w-96 h-96 rounded-xl">
+                        <h1 className="text-white">Nome do Projeto</h1>
+                        <p className="text-white">Descrição projeto</p>
+                    </li>
+
+                    <li className="bg-gray-900 w-96 h-96 rounded-xl">
+                        <h1 className="text-white">Nome do Projeto</h1>
+                        <p className="text-white">Descrição projeto</p>
+                    </li>
+
+                    <li className="bg-gray-900 w-96 h-96 rounded-xl">
+                        <h1 className="text-white">Nome do Projeto</h1>
+                        <p className="text-white">Descrição projeto</p>
+                    </li>
+                </ul>
             </div>
-            
+
         </div>
     )
 }
