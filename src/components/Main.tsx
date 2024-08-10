c82b8b4dfca09485f9f709e2c83565b1d3ae6c5f
abneralvesmuniz76@gmail.com <abneralvesmuniz76@gmail.com>
2024-08-10 15:44:48 -0300

    Adicionando Download Curriculo

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -40,7 +40,7 @@ export default function Main () {
                     
                     <div className='flex flex-col m-5 xl:ml-0 2xl:ml-0 xl:mr-8 2xl:mt-10' data-aos="fade-up">
                         <p className={`text-zinc-500 text-lg mb-2 ${Exo.className}`}>OBS: Você pode encontrar este projeto no <span className='text-blue-200 underline decoration-1'><a href="https://github.com/abneeralves/Portfolio" target="_blank" rel="noopener noreferrer">GitHub.</a></span></p>
-                        <a className={`flex items-center justify-center p-2 w-full rounded-md text-center text-white border-2 border-blue-500 hover:shadow-lg hover:shadow-blue-400 hover:bg-white hover:text-black ${Exo.className}`} href=""><span className='mr-1'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                        <a className={`flex items-center justify-center p-2 w-full rounded-md text-center text-white border-2 border-blue-500 hover:shadow-lg hover:shadow-blue-400 hover:bg-white hover:text-black ${Exo.className}`} href="/CurriculoAbner.png" download="CurriculoAbner.png"><span className='mr-1'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
                             <path d="M8.75 2.75a.75.75 0 0 0-1.5 0v5.69L5.03 6.22a.75.75 0 0 0-1.06 1.06l3.5 3.5a.75.75 0 0 0 1.06 0l3.5-3.5a.75.75 0 0 0-1.06-1.06L8.75 8.44V2.75Z" />
                             <path d="M3.5 9.75a.75.75 0 0 0-1.5 0v1.5A2.75 2.75 0 0 0 4.75 14h6.5A2.75 2.75 0 0 0 14 11.25v-1.5a.75.75 0 0 0-1.5 0v1.5c0 .69-.56 1.25-1.25 1.25h-6.5c-.69 0-1.25-.56-1.25-1.25v-1.5Z" />
                         </svg></span>Download Curriculo.</a>
