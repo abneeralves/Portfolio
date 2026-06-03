cb2728d599bf84c9c8ff45474d31ecf2173d72e0
Abner Alves <abneralvesmuniz76@gmail.com>
2026-06-03 13:39:48 -0300

    style: refine header styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer"
 
 import { useEffect } from "react"
 import AOS from 'aos'
-import 'aos/dist/aos.css'
+
 
 export default function Home() {
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,50 @@
 'use client'
+
 import Image from 'next/image'
-import LetraA from '../../public/A.png'
-
-import Whats from '../../public/whatsapp.png'
-import Instagram from '../../public/Instagram.png'
-import Twitter from '../../public/twitter.png'
-import Linkedin from '../../public/Linkedin.png'
-import GitHub from '../../public/GitHub.png'
-
-import { Exo_2, Anton, Fira_Code } from 'next/font/google'
 import { useState } from 'react'
-import { findSourceMap } from 'module'
-const Exo = Exo_2({ subsets: ['latin'] })
+import { Anton, Fira_Code } from 'next/font/google'
+
 const teko = Anton({ subsets: ['latin'], weight: '400' })
 const FiraCode = Fira_Code({ subsets: ['latin'] })
-const Header: React.FC = () => {
 
+export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const toggleMenu = () => { setIsMenuOpen(!isMenuOpen) }
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen)
+    type Names = {
+        firstName: string;
+        midName: string;
+        lastName: string;
+        firstNameProfession: string;
+        midNameProfession: string;
+        lastNameProfession: string;
     }
 
     const firstName = "ABNER";
     const midName = "ALVES";
     const lastName = "MUNIZ";
-    const full = "FULL";
-    const stack = "STACK";
-    const developer = "DEVELOPER";
+    const firstNameProfession = "FULL";
+    const midNameProfession = "STACK";
+    const lastNameProfession = "DEVELOPER";
 
-    const letterClass = `
-    text-8xl 2xl:text-[14rem] hover:-translate-y-2 hover:text-[#ffd401] hover:cursor-pointer duration-300 ease-in-out outline-name tracking-widest`;
+    const StyleClass =
+        `text-8xl 2xl:text-[14rem] 
+    hover:-translate-y-2 hover:text-[#ffd401] 
+    hover:cursor-pointer duration-300 
+    ease-in-out outline-name tracking-widest`;
 
-    const midClass = `
-    text-8xl 2xl:text-[14rem] text-[#ffd401] hover:-translate-y-2 hover:text-[#ffd401] hover:cursor-pointer duration-300 ease-in-out tracking-widest`;
+    const highlightStyleClass =
+        `text-8xl 2xl:text-[14rem] 
+    text-[#ffd401] hover:-translate-y-2 
+    hover:text-[#ffd401] hover:cursor-pointer 
+    duration-300 ease-in-out tracking-widest `;
 
     return (
         <header className="h-screen bg-[#171330] relative overflow-hidden">
-            <nav className="flex items-center justify-between px-10 md:px-14 pt-8 border-b border-white/5 pb-5 z-20 relative bg-[#171330]">
+            <nav className="fixed top-0 left-0 w-full flex items-center justify-between px-10 md:px-14 pt-8 border-b border-white/5 pb-5 z-20 bg-[#171330]">
                 <div className='flex items-center hover:rotate-180 transition-transform duration-500 cursor-pointer z-20'>
                     <p className='text-sky-500 text-2xl xl:text-3xl -m-1'>&lt;</p>
-                    <Image src={LetraA} alt='Letra A' width={30} />
+                    <Image src="/imgHeader/A.png" alt='Letra A' width={30} height={30} />
                     <p className='text-sky-500 text-2xl xl:text-3xl -m-1'>/&gt;</p>
                 </div>
 
@@ -72,21 +76,31 @@ const Header: React.FC = () => {
                 </button>
             </nav>
 
-            <div className='flex items-center justify-center md:justify-normal md:ml-28 lg:ml-44 xl:ml-14 2xl:-mt-20 relative' data-aos="fade-right">
+            <div className='flex items-center justify-center relative' data-aos="fade-right">
 
                 <div className='flex'>
                     <div>
                         <div className="flex">
                             {lastName.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
                         </div>
 
                         <div className="flex">
-                            {firstName.split("").map((letter) => (
-                                <p key={letter} className={`${midClass} ${teko.className}`}>
+                            {firstName.split("").map((letter, index) => (
+                                <p
+                                    key={index}
+                                    className={`
+                                        ${highlightStyleClass}
+                                        ${teko.className}
+                                        auto-hover
+                                    `}
+                                    style={{
+                                        animationDelay: `${index * 1}s`,
+                                    }}
+                                >
                                     {letter}
                                 </p>
                             ))}
@@ -94,7 +108,7 @@ const Header: React.FC = () => {
 
                         <div className="flex">
                             {midName.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
@@ -102,14 +116,15 @@ const Header: React.FC = () => {
 
                         <div className="flex">
                             {lastName.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
                         </div>
+
                         <div className="flex">
                             {firstName.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
@@ -118,40 +133,40 @@ const Header: React.FC = () => {
 
                     <div className='ml-20'>
                         <div className="flex">
-                            {developer.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                            {lastNameProfession.split("").map((letter) => (
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
                         </div>
 
                         <div className="flex">
-                            {full.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                            {firstNameProfession.split("").map((letter) => (
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
                         </div>
 
                         <div className="flex">
-                            {stack.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                            {midNameProfession.split("").map((letter) => (
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
                         </div>
 
                         <div className="flex">
-                            {developer.split("").map((letter) => (
-                                <p key={letter} className={`${midClass} ${teko.className}`}>
+                            {lastNameProfession.split("").map((letter) => (
+                                <p key={letter} className={`${highlightStyleClass} ${teko.className} tracking-[0.5rem]`}>
                                     {letter}
                                 </p>
                             ))}
                         </div>
 
                         <div className="flex">
-                            {full.split("").map((letter) => (
-                                <p key={letter} className={`${letterClass} ${teko.className}`}>
+                            {firstNameProfession.split("").map((letter) => (
+                                <p key={letter} className={`${StyleClass} ${teko.className}`}>
                                     {letter}
                                 </p>
                             ))}
@@ -162,5 +177,3 @@ const Header: React.FC = () => {
         </header>
     )
 }
-
-export default Header
